d29e06d452c1931943af8f1d5b3daa8c01be07fc
Stefan Weijers <stxfie@pm.me>
2022-10-06 03:03:23 +0200

    Added ABPlayer to app, i also made them fight hehe

--- src/NRow/App.java
+++ src/NRow/App.java
@@ -3,6 +3,7 @@ package NRow;
 import NRow.Heuristics.SimpleHeuristic;
 import NRow.Players.HumanPlayer;
 import NRow.Players.MinMaxPlayer;
+import NRow.Players.AlphaBetaPlayer;
 import NRow.Players.PlayerController;
 
 public class App {
@@ -27,8 +28,8 @@ public class App {
         SimpleHeuristic heuristic1 = new SimpleHeuristic(n);
         SimpleHeuristic heuristic2 = new SimpleHeuristic(n);
 
-        PlayerController human = new MinMaxPlayer(1, n, 5, heuristic1);
-        PlayerController human2 = new HumanPlayer(2, n, heuristic2);
+        PlayerController human = new AlphaBetaPlayer(1, n, 5, heuristic1);
+        PlayerController human2 = new MinMaxPlayer(2, n, 5, heuristic2);
 
         PlayerController[] players = { human, human2 };
 
